fd2cbb59fabfb4bf2d17157f8b2277a2fd8a98a1
Gopu2001 <41317321+Gopu2001@users.noreply.github.com>
2022-10-15 00:46:09 -0700

    ak - #9 initial reddit service controller

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/RedditController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/RedditController.java
@@ -2,9 +2,40 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.spring.backenddemo.services.RedditQueryService;
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(description="Subreddit info from Reddit")
+@Slf4j
 @RestController
+@RequestMapping("/api/reddit")
 public class RedditController {
     
+    ObjectMapper mapper = new ObjectMapper();
 
+    @Autowired
+    RedditQueryService redditQueryService;
+
+    @ApiOperation(value = "Get posts from a subreddit")
+    @GetMapping("/get")
+    public ResponseEntity<String> getSubredditData(
+        @ApiParam("subreddit, e.g. programminghumor") @RequestParam String subreddit
+    ) throws JsonProcessingException {
+        log.info("getSubredditData: subreddit={}", subreddit);
+        String result = redditQueryService.getJSON(subreddit);
+        return ResponseEntity.ok().body(result);
+    }
 }
